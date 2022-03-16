47ce0d02e52292145090e846b5007e33c6ecb4cf
Prateek Yadav <yprateek51@gmail.com>
2022-03-16 15:39:48 +0530

    service-registery started

---
+++ service-registery/src/main/java/com/serviceregistery/ServiceRegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.serviceregistery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisteryApplication.class, args);
+	}
+
+}
---
+++ service-registery/src/test/java/com/serviceregistery/ServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
